cdf6fcf35d7ccf55aca29e054e96dc2f8f1ba24a
nicasiomarques <nicasiomarques18@gmail.com>
2021-05-23 12:40:02 +0100

    exercicie 10

---
+++ exercises solved/ex11.java
@@ -0,0 +1,27 @@
+
+/**
+ * 1. Faça um programa que recebe uma quantidade de números determinados pelo
+ * utilizador e imprime-os de forma inversa.
+ */
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ex1 {
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
+    int num = 0;
+
+    do {
+      if (num < 0)
+        return;
+      System.out.println("Informe os numeros: ");
+      num = keyboard.nextInt();
+      numbers.add(num);
+    } while (num > 0);
+
+    for (int i = numbers.size() - 1; i >= 0; i--) {
+      System.out.println(numbers.get(i));
+    }
+  }
+}
